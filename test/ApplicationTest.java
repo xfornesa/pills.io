a9252ff667381737cf50afb03be2d0e24b834b59
Xavier Fornes <xfornesa@gmail.com>
2015-10-25 09:53:08 +0100

    fix deprecated play test helper contentType

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,23 +1,9 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import org.junit.*;
-
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
+import org.junit.Test;
 import play.twirl.api.Content;
 
-import static play.test.Helpers.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static play.test.Helpers.contentAsString;
 
 
 /**
@@ -37,7 +23,7 @@ public class ApplicationTest {
     @Test
     public void renderTemplate() {
         Content html = views.html.index.render("Your new application is ready.");
-        assertEquals("text/html", contentType(html));
+        assertEquals("text/html", html.contentType());
         assertTrue(contentAsString(html).contains("Your new application is ready."));
     }
 
