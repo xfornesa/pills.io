e84c26771e7d361cb79eb51a9b796fb6cd1cd6d9
Xavier Fornes <xfornesa@gmail.com>
2015-10-28 07:53:21 +0100

    getting data for step "Given the following pills" properly

--- test/com/prunatic/pills/cucumber/domain/pills/PillsSteps.java
+++ test/com/prunatic/pills/cucumber/domain/pills/PillsSteps.java
@@ -6,15 +6,18 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  */
 public class PillsSteps {
     @Given("^the following pills:$")
-    public void the_following_pills(DataTable data) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-        // E,K,V must be a scalar (String, Integer, Date, enum etc)
+    public void the_following_pills(List<Map<String, String>> rows) throws Throwable {
+        rows.parallelStream()
+                .forEach(row -> {
+                    System.out.println(String.format("Adding pill titled '%s' with content '%s'", row.get("title"), row.get("content")));
+                });
         throw new PendingException();
     }
 
