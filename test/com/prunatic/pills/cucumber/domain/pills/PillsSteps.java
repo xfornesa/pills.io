20204bd2d38f4d84ecd8505bc7b07dbb8370dd28
Xavier Fornes <xfornesa@gmail.com>
2015-10-28 07:43:47 +0100

    rewrite add pill feature and add steps as pending

--- test/com/prunatic/pills/cucumber/domain/pills/PillsSteps.java
+++ test/com/prunatic/pills/cucumber/domain/pills/PillsSteps.java
@@ -1,6 +1,35 @@
 package com.prunatic.pills.cucumber.domain.pills;
 
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 /**
  */
 public class PillsSteps {
+    @Given("^the following pills:$")
+    public void the_following_pills(DataTable data) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc)
+        throw new PendingException();
+    }
+
+    @When("^I get all the pills$")
+    public void i_get_all_the_pills() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I found the following pills:$")
+    public void i_found_the_following_pills(DataTable data) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc)
+        throw new PendingException();
+    }
 }
--- test/resources/com/prunatic/pills/cucumber/domain/features/pills/manage_pills.feature
+++ test/resources/com/prunatic/pills/cucumber/domain/features/pills/manage_pills.feature
@@ -2,10 +2,9 @@ Feature: Manage pills available within the system
 
 
   Scenario: Add pill to system
-    Given the following pill:
-      | title   | first pill of knowledge |
-      | content | Content of the pill     |
-    When I ask for all the pills
-    Then I will get the following pills:
+    Given the following pills:
+      | title                   | content             |
+      | first pill of knowledge | Content of the pill |
+    When I get all the pills
+    Then I found the following pills:
       | first pill of knowledge |
-
